4922819b75558bb3cd9c75c4161e8406c2aff387
zhoudongdong <976201076@qq.com>
2019-10-08 12:24:16 +0800

    更改gitignore

--- src/components/hello-word.spec.js
+++ src/components/hello-word.spec.js
@@ -5,7 +5,7 @@ describe('<HelloWorld/>',() =>{
   it('should render correct contents',()=>{
     const wrapper = shallowMount(HelloWorld);
     expect(wrapper.find('h1').text()).
-    toEqual('Welcome to Your Vue.js App');
+    toEqual('Welcome to Your Vue.js Ap');
   })
 
 })
